a54ff0d2b51d1fb09fed6d8cc0064e2ecd242bf3
Olivia593 <xwang99@stevens.edu>
2018-05-08 15:55:16 -0400

    update js file

--- JavaScript & jQuery/String.js
+++ JavaScript & jQuery/String.js
@@ -1,36 +1,38 @@
 // setInterval 
 
-// var interval = setInterval(function(){
-//     console.log("interval");
-// },1000);
+var interval = setInterval(function(){
+    console.log("interval");
+},1000);
 
-// setTimeout(function(){
-//     clearInterval(interval);
-// },5000);
+setTimeout(function(){
+    clearInterval(interval);
+},5000);
 
 // callback function
 
-// function fn(callbackFn){
-//     setTimeout(function(){
-//         callbackFn('Hello');
-//     },5000);  
-// }
+function fn(callbackFn){
+    setTimeout(function(){
+        callbackFn('Hello');
+    },5000);  
+}
 
-// fn(function(value){
-//     console.log(value);
-// });
+fn(function(value){
+    console.log(value);
+});
+
+// String
 
 var str = "The user firstname is Olivia, and user last name is Wang";
-// var counter = 0;
-// str = str.replace(new RegExp('user','g'),function(match){
-//     counter++;
-//     if(counter == 2){
-//         return 'Olivia';
-//     }else{
-//         return match;
-//     }
-// });
-// console.log(str);
+var counter = 0;
+str = str.replace(new RegExp('user','g'),function(match){
+    counter++;
+    if(counter == 2){
+        return 'Olivia';
+    }else{
+        return match;
+    }
+});
+console.log(str);
 
 if(str.match('Olivia')){
     console.log('Match found');
