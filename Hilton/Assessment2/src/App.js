5e615d4ac9e569f106bebec5143eb294560ffa13
Olivia593 <xwang99@stevens.edu>
2018-07-25 11:16:45 -0400

    update react

--- Hilton/Assessment2/src/App.js
+++ Hilton/Assessment2/src/App.js
@@ -97,15 +97,6 @@ class App extends Component {
     <div>
       <button style={{position: "absolute", top:"150px", left: "40px"}}>Submit</button>
     </div></div>);
-      // <div className="App">
-      //   <header className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <h1 className="App-title">Welcome to React</h1>
-      //   </header>
-      //   <p className="App-intro">
-      //     To get started, edit <code>src/App.js</code> and save to reload.
-      //   </p>
-      // </div>
       
   }
 }
